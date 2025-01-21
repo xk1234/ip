1b2f4dcd829df576cd1d6a300ddd01e13cf1c519
Ye Xinkang <yexinkang1234@gmail.com>
2025-01-21 22:20:31 +0800

    Added level-3

---
+++ AI.md
@@ -0,0 +1,3 @@
+Level 0-2: No AI
+
+AI model used: CHatGPT o4 / o1 model
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,24 +11,56 @@ public class Duke {
         String name = "Duke";
 
         System.out.println("Hello! I'm " + name + "\n");
-        System.out.println("What can I do for you?\n");
         ArrayList<String> list = new ArrayList<>();
+        ArrayList<Boolean> done = new ArrayList<>();
+
         while (true) {
+            System.out.println("What can I do for you?\n");
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
+
             if (input.equalsIgnoreCase("bye")) {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                Integer ctr = 0;
-                for (String s : list) {
-                    System.out.println(ctr + ". " + s);
-                    ctr++;
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < list.size(); i++) {
+                    String status = done.get(i) ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + ". " + status + " " + list.get(i));
+                }
+            } else if (input.startsWith("mark")) {
+                try {
+                    int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (taskNumber >= 0 && taskNumber < list.size()) {
+                        done.set(taskNumber, true);
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  [X] " + list.get(taskNumber));
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Use: mark <task_number>");
+                }
+            } else if (input.startsWith("unmark")) {
+                try {
+                    int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (taskNumber >= 0 && taskNumber < list.size()) {
+                        done.set(taskNumber, false);
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  [ ] " + list.get(taskNumber));
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Use: unmark <task_number>");
                 }
             } else {
                 list.add(input);
+                done.add(false);
             }
+            System.out.println();
         }
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
 
     }
 }
