6e1308fcc7ff3e4ba73de210010e80c46940d623
Ye Xinkang <yexinkang1234@gmail.com>
2025-01-21 22:07:21 +0800

    Added level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -11,13 +12,20 @@ public class Duke {
 
         System.out.println("Hello! I'm " + name + "\n");
         System.out.println("What can I do for you?\n");
+        ArrayList<String> list = new ArrayList<>();
         while (true) {
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                Integer ctr = 0;
+                for (String s : list) {
+                    System.out.println(ctr + ". " + s);
+                    ctr++;
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
             }
         }
         System.out.println("Bye. Hope to see you again soon!\n");
