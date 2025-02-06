191eee8db3f86b88474e674f0e4841f54767d114
Ye Xinkang <yexinkang1234@gmail.com>
2025-02-06 12:52:05 +0800

    Add A-CheckStyle

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,4 +1,5 @@
 package duke;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -15,7 +16,8 @@ class Parser {
      * Splits the input line by space to separate the command keyword from the arguments.
      *
      * @param inputLine The full input line from the user.
-     * @return An array of strings, where the first element is the command and the second element is the arguments (can be null if no arguments).
+     * @return An array of strings, where the first element is the command \
+     *          and the second element is the arguments (can be null if no arguments).
      */
     public static String[] parseCommand(String inputLine) {
         return inputLine.split(" ", 2);
@@ -43,7 +45,8 @@ class Parser {
      * Splits the arguments string by " /by " to separate the description from the deadline time string.
      *
      * @param arguments The arguments string for the deadline command.
-     * @return An array of strings, where the first element is the description and the second element is the deadline time string.
+     * @return An array of strings, where the first element is the description and the second \
+     *          element is the deadline time string.
      */
     public static String[] parseDeadlineArguments(String arguments) {
         return arguments.split(" /by ", 2);
@@ -51,10 +54,12 @@ class Parser {
 
     /**
      * Parses the arguments for an event command.
-     * Splits the arguments string by " /from " and " /to " to separate the description, from time string, and to time string.
+     * Splits the arguments string by " /from " and " /to " to separate the description, \
+     *          from time string, and to time string.
      *
      * @param arguments The arguments string for the event command.
-     * @return An array of strings, where the first element is the description, the second is the from time string, and the third is the to time string.
+     * @return An array of strings, where the first element is the description, \
+     *          the second is the from time string, and the third is the to time string.
      */
     public static String[] parseEventArguments(String arguments) {
         return arguments.split(" /from | /to ");
